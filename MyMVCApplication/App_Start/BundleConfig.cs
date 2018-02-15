bc7d45e6186de65b1b61d1fa3e882df73126ec57
purjeet das <purjeet@purjeets-mbp.attlocal.net>
2018-02-14 23:08:50 -0500

    Added Popup feature

--- MyMVCApplication/App_Start/BundleConfig.cs
+++ MyMVCApplication/App_Start/BundleConfig.cs
@@ -18,10 +18,13 @@ namespace MyMVCApplication
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery/jquery.js"));
             bundles.Add(new StyleBundle("~/Content/kendo/css").Include(
-                "~/Content/kendo/kendo.common-material.min.css",
-                "~/Content/kendo/kendo.rtl.min.css",
+                "~/Content/kendo/kendo.common.min.css",
                 "~/Content/kendo/kendo.material.mobile.min.css",
                "~/Content/kendo/kendo.material.min.css"));
+            bundles.Add(new StyleBundle("~/Content/Bootstrap/css").Include(
+                "~/Content/Bootstrap/bootstrap.min.css"));
+            bundles.Add(new StyleBundle("~/Content/App/css").Include(
+               "~/Content/MyMVCApplication/app.css"));
             bundles.IgnoreList.Clear();
         }
     }
